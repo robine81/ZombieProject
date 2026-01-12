93ee330b5f2639b2c517058a65235185b864a22e
robine81 <robine.westberg@gmail.com>
2026-01-12 11:28:00 +0100

    Initial commit with SpringBoot Initializr

---
+++ src/main/java/com/lexicon/ZombieProject/ZombieProjectApplication.java
@@ -0,0 +1,13 @@
+package com.lexicon.ZombieProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZombieProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZombieProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lexicon/ZombieProject/ZombieProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lexicon.ZombieProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZombieProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
