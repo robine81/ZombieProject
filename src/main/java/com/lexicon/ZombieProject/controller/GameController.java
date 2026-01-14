f44fc17ea43f8e20e87ad8c51769f759d26789cd
Tamas Tazlo <tazlo.tamas@gmail.com>
2026-01-14 16:14:47 +0100

    Configure tests to run with in-memory DB

---
+++ src/main/java/com/lexicon/ZombieProject/controller/GameController.java
@@ -0,0 +1,15 @@
+package com.lexicon.ZombieProject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/game")
+public class GameController {
+
+    @GetMapping
+    public String hello() {
+        return "Hello world";
+    }
+}
--- src/test/java/com/lexicon/ZombieProject/ZombieProjectApplicationTests.java
+++ src/test/java/com/lexicon/ZombieProject/ZombieProjectApplicationTests.java
@@ -2,8 +2,10 @@ package com.lexicon.ZombieProject;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ZombieProjectApplicationTests {
 
 	@Test
