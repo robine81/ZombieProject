f3ef820023db059bfaeba3681aed937769ffadfb
Jonathan Rubensson <jonathan@eldfluga.se>
2026-01-15 14:49:53 +0100

    Add api end points for service methods.

--- src/main/java/com/lexicon/ZombieProject/controller/GameController.java
+++ src/main/java/com/lexicon/ZombieProject/controller/GameController.java
@@ -1,6 +1,9 @@
 package com.lexicon.ZombieProject.controller;
 
+import com.lexicon.ZombieProject.service.GameService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +11,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/game")
 public class GameController {
 
+    private final GameService service;
+
+    public GameController(GameService service){
+        this.service = service;
+    }
+
     @GetMapping
-    public String hello() {
-        return "Hello world";
+    public ResponseEntity<?> getCurrentScene(){
+        return ResponseEntity.ok(service.getCurrentScene());
+    }
+
+    @GetMapping("/{optionId}")
+    public ResponseEntity<?> selectOption(@PathVariable int optionId){
+        return ResponseEntity.ok(service.executeTransition(optionId));
     }
 }
